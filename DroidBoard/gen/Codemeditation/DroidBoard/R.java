76d23be06e57cbb8b48adf68c16f37cfa1a03ff4
MonsterCoder <MonsterCoder@gmail.com>
2011-04-18 01:25:33 -0500

    adding project activity

--- DroidBoard/gen/Codemeditation/DroidBoard/R.java
+++ DroidBoard/gen/Codemeditation/DroidBoard/R.java
@@ -24,11 +24,13 @@ public final class R {
         public static final int description=0x7f070001;
         public static final int icon=0x7f070002;
         public static final int name=0x7f070000;
-        public static final int projectlist_title=0x7f070005;
+        public static final int project_header=0x7f070005;
+        public static final int projectlist_title=0x7f070006;
     }
     public static final class layout {
         public static final int project_item=0x7f030000;
-        public static final int projectlistview=0x7f030001;
+        public static final int project_view=0x7f030001;
+        public static final int projectlistview=0x7f030002;
     }
     public static final class string {
         public static final int app_description=0x7f050003;
