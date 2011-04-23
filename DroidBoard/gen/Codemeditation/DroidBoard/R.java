8d168133a9f886c0a10fdbd21ba5c01b813389ac
MonsterCoder <MonsterCoder@gmail.com>
2011-04-23 00:31:32 -0500

    added stories

--- DroidBoard/gen/Codemeditation/DroidBoard/R.java
+++ DroidBoard/gen/Codemeditation/DroidBoard/R.java
@@ -25,10 +25,10 @@ public final class R {
         public static final int app_lable=0x7f070003;
         public static final int description=0x7f070001;
         public static final int icon=0x7f070002;
-        public static final int id=0x7f070006;
         public static final int name=0x7f070000;
         public static final int projectlist_title=0x7f070005;
         public static final int text=0x7f070007;
+        public static final int title=0x7f070006;
     }
     public static final class layout {
         public static final int phases_list_view=0x7f030000;
