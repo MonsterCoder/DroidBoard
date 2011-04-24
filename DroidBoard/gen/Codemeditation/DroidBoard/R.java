b323c7aeff959e75517b63c54308e45b9171b2a0
MonsterCoder <MonsterCoder@gmail.com>
2011-04-24 11:22:38 -0500

    changed background

--- DroidBoard/gen/Codemeditation/DroidBoard/R.java
+++ DroidBoard/gen/Codemeditation/DroidBoard/R.java
@@ -18,27 +18,29 @@ public final class R {
     }
     public static final class drawable {
         public static final int app_notes=0x7f020000;
-        public static final int ic_menu_compose=0x7f020001;
-        public static final int ic_menu_delete=0x7f020002;
-        public static final int ic_menu_edit=0x7f020003;
-        public static final int ic_menu_revert=0x7f020004;
-        public static final int ic_menu_save=0x7f020005;
-        public static final int icon=0x7f020006;
-        public static final int kanban_logo=0x7f020007;
-        public static final int live_folder_notes=0x7f020008;
-        public static final int meditation=0x7f020009;
-        public static final int story_background=0x7f02000a;
-        public static final int woodboard=0x7f02000b;
+        public static final int canstock=0x7f020001;
+        public static final int ic_menu_compose=0x7f020002;
+        public static final int ic_menu_delete=0x7f020003;
+        public static final int ic_menu_edit=0x7f020004;
+        public static final int ic_menu_revert=0x7f020005;
+        public static final int ic_menu_save=0x7f020006;
+        public static final int icon=0x7f020007;
+        public static final int kanban_logo=0x7f020008;
+        public static final int live_folder_notes=0x7f020009;
+        public static final int meditation=0x7f02000a;
+        public static final int story_background=0x7f02000b;
+        public static final int woodboard=0x7f02000c;
+        public static final int woodtag=0x7f02000d;
     }
     public static final class id {
         public static final int app_description=0x7f070006;
         public static final int app_lable=0x7f070005;
-        public static final int description=0x7f070004;
-        public static final int icon=0x7f070000;
-        public static final int name=0x7f070003;
-        public static final int phase_name=0x7f070001;
+        public static final int description=0x7f070003;
+        public static final int icon=0x7f070004;
+        public static final int name=0x7f070002;
+        public static final int phase_name=0x7f070000;
         public static final int projectlist_title=0x7f070007;
-        public static final int story_list=0x7f070002;
+        public static final int story_list=0x7f070001;
         public static final int text=0x7f070009;
         public static final int title=0x7f070008;
     }
